99cb655a9feb33cfe51ecaf9c8518530501f0364
bobtheshrew <anker@capital.net>
2013-06-21 16:50:57 -0400

    moved envVar replace to just get passed to evaluateGroovyScriptFilePath
    not great, but might compile this time...

--- src/main/java/org/jenkinsci/plugins/scripttrigger/groovy/GroovyScriptTrigger.java
+++ src/main/java/org/jenkinsci/plugins/scripttrigger/groovy/GroovyScriptTrigger.java
@@ -138,7 +138,6 @@ public class GroovyScriptTrigger extends AbstractTrigger {
             Map<String, String> envVars;
             try {
                 envVars = envVarsResolver.getPollingEnvVars(proj, pollingNode);
-    			groovyFilePath = Util.replaceMacro(groovyFilePath, envVars);
             } catch (EnvInjectException e) {
                 throw new ScriptTriggerException(e);
             }
@@ -151,7 +150,7 @@ public class GroovyScriptTrigger extends AbstractTrigger {
             }
 
             if (groovyFilePath != null) {
-                boolean evaluationSucceed = executor.evaluateGroovyScriptFilePath(pollingNode, proj, groovyFilePath, envVars, groovySystemScript);
+                boolean evaluationSucceed = executor.evaluateGroovyScriptFilePath(pollingNode, proj, Util.replaceMacro(groovyFilePath, envVars), envVars, groovySystemScript);
                 if (evaluationSucceed) {
                     return true;
                 }
